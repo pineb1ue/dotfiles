57a1f9a908ab41a3ff1db41575c2082f172593e2
Mamoru Matsunaga <pineb1ue10ngtree@gmail.com>
2021-11-11 19:43:41 +0900

    Update for M1 MacBookAir

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,7 +9,7 @@ function command_exists {
 # homebrew
 if ! command_exists brew ; then
   echo " --------- Homebrew ----------"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew update
   brew upgrade --all --cleanup
   brew -v
