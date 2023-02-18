424373762aa497b81c9536e6b05ad03cae124eb1
Mamoru Matsunaga <pineb1ue10ngtree@gmail.com>
2023-02-18 13:28:45 +0900

    fix: fix font name

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require "wezterm"
 return {
     use_ime = ture,
     default_cwd = "/Users/pineb1ue",
-    font = wezterm.font("HackGen 35Console NF"),
+    font = wezterm.font("HackGen35 Console NF"),
     font_size = 14,
     color_scheme = "AyuDark (Gogh)",
     windows_background_opacity = 0.9,
