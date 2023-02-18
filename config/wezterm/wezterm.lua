d9c695c4fbb83c95d2d6fb222a2cd74102d5cfb8
Mamoru Matsunaga <pineb1ue10ngtree@gmail.com>
2023-02-18 13:22:01 +0900

    chore: change wezterm font

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local wezterm = require "wezterm"
 return {
     use_ime = ture,
     default_cwd = "/Users/pineb1ue",
-    font = wezterm.font("HackGen Console NF"),
-    font_size = 12,
+    font = wezterm.font("HackGen 35Console NF"),
+    font_size = 14,
     color_scheme = "AyuDark (Gogh)",
     windows_background_opacity = 0.9,
 }
